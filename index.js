d140409c090ee32c76bb3f4704bcef26fa3cdbcf
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-05-25 17:28:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", function() {
     updateDOM()
 } );
@@ -7,12 +8,22 @@ function updateDOM() {
     .innerHTML = "This is really cool!";
 }
       document.addEventListener("DOMContentLoaded", function() {
+=======
+    document.addEventListener("DOMContentLoaded", function() {
+>>>>>>> 3e7b2c387296ccbf00ddf5211262d0c89735f1b3
       console.log("The DOM has loaded");
     });
      
     console.log(
       "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
     );
+<<<<<<< HEAD
     function myFunction() {
         document.getElementById("text").innerHTML = "Hello World";
       }
+=======
+function updateDOM() {
+    document.getElementById('text')
+    .innerHTML = "This is really cool!";
+}
+>>>>>>> 3e7b2c387296ccbf00ddf5211262d0c89735f1b3
